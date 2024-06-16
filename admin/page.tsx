3a5422d527ba86f1cc0150298bdd6951477ccea5
KingBonfante <72134861+KingBonfante@users.noreply.github.com>
2024-06-16 22:04:37 +0000

    feat: configuração de ambiente

---
+++ admin/page.tsx
@@ -0,0 +1,14 @@
+// import { Button } from "@/components/ui/button";
+// import { MenuSheet } from "./components/MenuSheet";
+
+export default function Admin() {
+    return (
+        <div>
+            <h1 className="text-center">Bem Vindo à Área Administrativa</h1>
+           
+            {/* <MenuSheet/> */}
+           
+        </div>
+
+    )
+}
